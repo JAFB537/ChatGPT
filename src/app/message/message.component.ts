2e5b1d9e8a6f57ba5f8969329a89eb4e232fb1b6
JAFB537 <jafb537@gmail.com>
2024-07-29 12:29:58 -0600

    Add Message

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css',
+})
+export class MessageComponent {
+  @Input() sender: string = '' 
+  @Input() content: string = ''
+
+  now = new Date().toLocaleDateString('es-ES', {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  date = this.now;
+}
