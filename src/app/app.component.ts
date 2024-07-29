53c47e097ec4b7f04a1d6ecaeb77dec23dd93a2c
JAFB537 <jafb537@gmail.com>
2024-07-29 12:30:16 -0600

    Update App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, afterRender } from '@angular/core';
+import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, CommonModule, RouterOutlet, HeaderComponent, FooterComponent, ContentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ChatGPT';
+  constructor(@Inject(PLATFORM_ID) private platformId:Object){
+    this.platformId = platformId;
+    if(isPlatformBrowser(platformId)){
+      // runs on client / browser
+    }
+    if(isPlatformServer(platformId)){
+      // runs on server / node
+    }  
+  } 
+
+  ngOnInit(): void {
+    if(isPlatformBrowser(this.platformId)){
+      console.log("Output is generated only in browser not in server.");
+    }
+    if(isPlatformServer(this.platformId)){
+      console.log("Output is generated only in server not in browser.");
+    }
+  }
 }
